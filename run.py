ec3da2fd0fcae647de0f6c2639664b7eedd1aa8a
Andrew Sonnier <absonnie@ncsu.edu>
2019-11-07 15:27:28 -0500

    Added import_file function to import json file with interop data

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
+import json
+
+
 def run():
     print('Hello World')
+
+def import_file(file):
+    interop_data = json.load(file)
+    print(interop_data)
 
 if __name__ == "__main__":
     run()
