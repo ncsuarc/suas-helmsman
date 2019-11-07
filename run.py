b2100c8980585b3b568e44a1fef5df41cd511592
Andrew Sonnier <absonnie@ncsu.edu>
2019-11-07 15:52:12 -0500

    Added json parser from -f tag to input interop data

--- run.py
+++ run.py
@@ -1,12 +1,19 @@
 import json
+import sys, argparse
 
-
-def run():
-    print('Hello World')
+file = None
 
 def import_file(file):
     interop_data = json.load(file)
-    print(interop_data)
+    print(interop_data["waypoints"])
+
+def handle_args():
+    global file
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', help='Path of interop file')
+    parsed_args = parser.parse_args(sys.argv[1:])
+    file = open(parsed_args.file)
 
 if __name__ == "__main__":
-    run()
+    handle_args()
+    import_file(file)
