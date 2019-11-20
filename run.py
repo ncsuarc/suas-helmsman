cf3b322e9a2bf9d0dfe13fa784e44e40a294579b
Andrew Sonnier <absonnie@ncsu.edu>
2019-11-19 19:12:41 -0500

    Added waypoint addition to mavlink

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import json
 import sys, argparse
-import pymavlink
+from pymavlink import mavwp
 from src.waypoint import Waypoint
 
+manager = mavwp.MAVWPLoader()
 
 def import_file(file):
     interop_data = json.load(file)
@@ -10,7 +11,7 @@ def import_file(file):
 
 def handle_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--file', help='Path of interop file')
+    parser.add_argument('-f', '--file', help='Path of interop file', required=True)
     parsed_args = parser.parse_args(sys.argv[1:])
     return open(parsed_args.file)
 
@@ -23,9 +24,13 @@ def get_waypoints(interop_data):
         way.append(Waypoint(lat, lon, alt))
     return way
 
+def add_waypoints(way):
+    for w in way:
+        manager.add_latlonalt(w.longitude,w.latitude,w.altitude) 
 
 if __name__ == "__main__":
     file = handle_args()
     interop_data = import_file(file)
     way = get_waypoints(interop_data)
-    print(way)
+    add_waypoints(way)
+    print(manager.count())
