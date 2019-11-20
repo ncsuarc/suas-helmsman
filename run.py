78e75f9678b22300ddad1308566fc06df7eabf62
Andrew Sonnier <absonnie@ncsu.edu>
2019-11-19 20:12:18 -0500

    Added upload to localhost plane via mavlink

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import json
 import sys, argparse
-from pymavlink import mavwp
+from pymavlink import mavwp, mavutil
 from src.waypoint import Waypoint
 
 manager = mavwp.MAVWPLoader()
@@ -26,11 +26,21 @@ def get_waypoints(interop_data):
 
 def add_waypoints(way):
     for w in way:
-        manager.add_latlonalt(w.longitude,w.latitude,w.altitude) 
+        manager.add_latlonalt(w.latitude,w.longitude,w.altitude) 
+
+def send_waypoints():
+    master = mavutil.mavlink_connection('udp:localhost:14551')  
+    master.wait_heartbeat(blocking=True) 
+    master.waypoint_clear_all_send()                                     
+    master.waypoint_count_send(manager.count())                          
+
+    for i in range(manager.count()):
+            msg = master.recv_match(type=['MISSION_REQUEST'],blocking=True)             
+            master.mav.send(manager.wp(msg.seq))                                                                          
 
 if __name__ == "__main__":
     file = handle_args()
     interop_data = import_file(file)
     way = get_waypoints(interop_data)
     add_waypoints(way)
-    print(manager.count())
+    send_waypoints()
