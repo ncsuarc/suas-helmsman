a41d032f0cbe888523577c233b2ea9419d0048d9
Andrew Sonnier <absonnie@ncsu.edu>
2020-01-11 10:58:10 -0500

    Additional command line args (#9)

--- run.py
+++ run.py
@@ -18,8 +18,8 @@ def add_waypoints(way):
         manager.add_latlonalt(w.latitude, w.longitude, w.altitude)
 
 
-def send_waypoints():
-    master = mavutil.mavlink_connection("udp:localhost:14551")
+def send_waypoints(ip):
+    master = mavutil.mavlink_connection("udp:" + ip)
     master.wait_heartbeat(blocking=True)
     master.waypoint_clear_all_send()
     master.waypoint_count_send(manager.count())
@@ -27,15 +27,31 @@ def send_waypoints():
     for _ in range(manager.count()):
         msg = master.recv_match(type=["MISSION_REQUEST"], blocking=True)
         master.mav.send(manager.wp(msg.seq))
+        master.mav.mount_control_send()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", help="Path of interop file", required=True)
+    parser.add_argument(
+        "-f",
+        "--file",
+        help="Path of interop file",
+        default="./test-files/suas_2019_missions.txt",
+    )
+    parser.add_argument(
+        "-i",
+        "--ip",
+        help="IP of the pixhawk to transmit data to",
+        default="localhost:14551",
+    )
     parsed_args = parser.parse_args()
-    with open(parsed_args.file, "r") as json_file:
+
+    interop_file = parsed_args.file
+    plane_ip = parsed_args.ip
+
+    with open(interop_file, "r") as json_file:
         interop_data = json.load(json_file)
 
     way = get_waypoints(interop_data)
     add_waypoints(way)
-    send_waypoints()
+    send_waypoints(plane_ip)
