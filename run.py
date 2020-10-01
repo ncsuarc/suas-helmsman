71fbe333396cd76977d8bbc0775155063c7392fe
Sam Stone <samjstone01@gmail.com>
2020-10-01 19:42:18 -0400

    removed pymavlink

--- run.py
+++ run.py
@@ -3,43 +3,12 @@ import json
 import sys
 import time
 
-from pymavlink import mavutil, mavwp
+
 
 from autogen import suas_graph
 
 
-def upload_waypoints(graph, ip):
-    """Function to upload the waypoints to the plane.
 
-    Uses pymavlink to upload the generated flight path from the NetworkX graph
-    Must be run after construct_graph() if using the autogen generator. This adds
-    all waypoints to the mavlink waypoint manager, establishes a connect with the plane
-    and then sends each waypoint to the plane.
-
-    Args:
-        graph (SUASGraph): An instance of SUASGraph
-        ip: the ip of the plane
-    """
-    # Construct Waypoint Manager
-    manager = mavwp.MAVWPLoader()
-    # Add inital point to manager, voided and not sent to plane
-    manager.add_latlonalt(
-        graph.starting_point["latitude"], graph.starting_point["longitude"], 150
-    )
-    # Adds each waypoint to the waypoint manager
-    for p in graph.path_lat_lon_alt():
-        manager.add_latlonalt(*p)
-
-    # Establish connect to plane at ip
-    master = mavutil.mavlink_connection("udp:" + ip)
-    master.wait_heartbeat(blocking=True)
-    master.waypoint_clear_all_send()
-    master.waypoint_count_send(manager.count())
-
-    # Send each point as requested
-    for _ in range(manager.count()):
-        msg = master.recv_match(type=["MISSION_REQUEST"], blocking=True)
-        master.mav.send(manager.wp(msg.seq))
 
 
 def construct_graph(interop_data, drop, off_axis, obstacles):
@@ -127,6 +96,6 @@ if __name__ == "__main__":
     )
     time2 = time.process_time()
     print("Graph: ", (time2 - time1))
-
+    print(graph.path)
     # Upload Flight Path
-    upload_waypoints(graph, parsed_args.ip)
+    
